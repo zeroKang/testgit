41350b371a6e829abb6bd23a871ff17175ff5ad1
unknown <cookieKang@gmail.com>
2019-12-12 15:37:32 +0900

    test

--- src/main/java/org/zerock/b1/DemoApplication.java
+++ src/main/java/org/zerock/b1/DemoApplication.java
@@ -12,6 +12,7 @@ public class DemoApplication {
 		System.out.println("AAAAAAAAA");
 		System.out.println("AAAAAAAAA");
 		System.out.println("AAAAAAAAA");
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
