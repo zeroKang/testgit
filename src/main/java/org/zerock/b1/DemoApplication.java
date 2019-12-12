20d70dc8e5ddd736cc66d01de9cc0b6b70c1a348
zeroKang <cookieKang@gmail.com>
2019-12-12 15:38:51 +0900

    second

--- src/main/java/org/zerock/b1/DemoApplication.java
+++ src/main/java/org/zerock/b1/DemoApplication.java
@@ -8,10 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("AAAAAAAAA");
-		System.out.println("AAAAAAAAA");
-		System.out.println("AAAAAAAAA");
-		System.out.println("AAAAAAAAA");
+		System.out.println("111");
+		System.out.println("22");
+		System.out.println("33");
+		System.out.println("44");
 		
 		SpringApplication.run(DemoApplication.class, args);
 	}
