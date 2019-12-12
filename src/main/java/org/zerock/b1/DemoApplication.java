9177c5cab4e3159e3ba4bb03e5ad42870177b112
zeroKang <cookieKang@gmail.com>
2019-12-12 16:39:04 +0900

    efzefe

--- src/main/java/org/zerock/b1/DemoApplication.java
+++ src/main/java/org/zerock/b1/DemoApplication.java
@@ -8,14 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("aaa");
-		System.out.println("bbb");
-		System.out.println("ccc");
-		System.out.println("ddd");
 
-		System.out.println("eee");
-
-		System.out.println("fff");
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
