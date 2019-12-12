59966486fb7d22af4977b3fddbede7fe69a2f9b0
zeroKang <cookieKang@gmail.com>
2019-12-12 15:41:52 +0900

    5th

--- src/main/java/org/zerock/b1/DemoApplication.java
+++ src/main/java/org/zerock/b1/DemoApplication.java
@@ -7,14 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
-
-
 		
-		System.out.println("aaaa");
-		System.out.println("bbb");
-		System.out.println("ccc");
-		System.out.println("ddd");
+		System.out.println("111");
+		System.out.println("222");
+		System.out.println("333");
+		System.out.println("444");
 		
 		SpringApplication.run(DemoApplication.class, args);
 	}
